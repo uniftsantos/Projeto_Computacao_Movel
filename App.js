662159d06188fb303a3687e97be248a7e415c95a
Tiago Fagundes dos Santos <146483236+uniftsantos@users.noreply.github.com>
2024-11-15 14:39:23 -0300

    Inserindo funcao de cadastro e login

--- App.js
+++ App.js
@@ -73,7 +73,7 @@ class Login extends React.Component {
 //   }
 // }
 
-// Tela de Cadastro
+// Fazendo tela de cadastro
 class Cadastro extends React.Component {
   constructor(props) {
     super(props);

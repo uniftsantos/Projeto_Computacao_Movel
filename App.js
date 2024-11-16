bf254ee63f5aa75f14cdfae96d16508a3546a37e
Tiago Fagundes dos Santos <146483236+uniftsantos@users.noreply.github.com>
2024-11-15 21:58:46 -0300

    Update App.js
    colocando livros para que o usuario possa comentar

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import * as React from 'react';
-import { TextInput, Text, View, Button, FlatList } from 'react-native';
+import { TextInput, Text, View, Button, FlatList, Image,ScrollView  } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import {MaterialCommunityIcons} from '@expo/vector-icons';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
+
 const Tab = createBottomTabNavigator();
+
 
 // Fazendo a tela de login.
 class Login extends React.Component {
@@ -26,8 +28,14 @@ class Login extends React.Component {
         <Text>{"Digite sua senha:"}</Text>
         <TextInput onChangeText = {(texto)=>this.setState({senha:texto})} secureTextEntry={true}></TextInput>
         <Button title= "Logar" onPress={()=>this.ler()}></Button>
+        <Text>{"Caso não tenha conta, clique para se cadastrar"}</Text>
+        <Button title= "Criar Conta" onPress={() =>this.manda_para_cadastro()}></Button>
       </View>
     )
+  }
+
+  manda_para_cadastro(){
+    this.props.navigation.navigate("Criar Usuário");
   }
 
   async ler(){
@@ -36,6 +44,8 @@ class Login extends React.Component {
       if(senha_login != null){
         if(senha_login == this.state.senha){
           alert("Usuário Logado !");
+          await AsyncStorage.setItem('usuario_logado', 'true');//dando permissao para o usuario poder mexer na pagina.
+          this.props.navigation.navigate("Livros");
         }
         else{
           alert("Senha Incorreta !");
@@ -57,8 +67,8 @@ class Login extends React.Component {
 //     if (senha != null) {
 //       if (senha == this.state.senha) {
 //         alert("Logado!");
-//         //await AsyncStorage.setItem('isLogged', 'true'); // Seta o login no AsyncStorage
-//         this.props.navigation.navigate("Filmes");
+//         //await AsyncStorage.setItem('usuario_logado', 'true'); // Seta o login no AsyncStorage
+//         this.props.navigation.navigate("Livros");
 //         } 
 //           else {
 //             alert("Senha Incorreta!");
@@ -73,7 +83,7 @@ class Login extends React.Component {
 //   }
 // }
 
-// Fazendo tela de cadastro
+// Fazedo Tela de Cadastro
 class Cadastro extends React.Component {
   constructor(props) {
     super(props);
@@ -86,7 +96,8 @@ class Cadastro extends React.Component {
   async salvar() {
     try {
       await AsyncStorage.setItem(this.state.user, this.state.password);
-      alert("Salvo com sucesso!!!");
+      alert("Cadastro Realizado");
+      
     } catch (erro) {
       alert("Erro!");
     }
@@ -96,77 +107,105 @@ class Cadastro extends React.Component {
     return (
       <View>
         <Text>{"Cadastrar Usuário:"}</Text>
-        <TextInput onChangeText={(texto) => this.setState({ user: texto })} />
+        <TextInput onChangeText={(texto) => this.setState({ user: texto })}></TextInput>
         <Text>{"Cadastrar Senha:"}</Text>
-        <TextInput onChangeText={(texto) => this.setState({ password: texto })} secureTextEntry={true} />
-        <Button title="Cadastrar" onPress={() => this.salvar()} />
+        <TextInput onChangeText={(texto) => this.setState({ password: texto })} secureTextEntry={true}></TextInput>
+        <Button title="Cadastrar" onPress={() => this.salvar()}></Button>
       </View>
-    );
+    )
   }
 }
 
-// Tela de Filmes com sistema de comentários
-class Filmes extends React.Component {
+class Livros extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isLogged: false,
-      comment: '',
-      comments: {},
-    };
-  }
-
-  async componentDidMount() {
-    const isLogged = await AsyncStorage.getItem('isLogged');
-    if (isLogged !== 'true') {
-      alert("Por favor, faça login para acessar esta página.");
-      this.props.navigation.navigate("Login");
-    } else {
-      this.setState({ isLogged: true });
+      usuario_logado: false, // Verifica se o usuário está logado
+      comment: '', // Guarda o texto do comentário
+      comments: {}, // Armazena os comentários por filme
     }
   }
 
+  // Função chamada ao carregar a tela para verificar se o usuário está logado
+  async componentDidMount() {
+    const usuario_logado = await AsyncStorage.getItem('usuario_logado');
+    if (usuario_logado !== 'true') {
+      alert("Necessário fazer login para acessar essa página");
+      // Se não estiver logado, pode redirecionar para a página de login
+      this.props.navigation.navigate("Login");
+    } else {
+      this.setState({ usuario_logado: true });
+    }
+  }
+
+  // Função para adicionar comentário para um filme específico
   addComment(movieId) {
     this.setState((prevState) => ({
       comments: {
         ...prevState.comments,
         [movieId]: (prevState.comments[movieId] || []).concat(prevState.comment),
       },
-      comment: '',
+      comment: '', // Limpa o campo de comentário após enviar
     }));
   }
 
   render() {
-    if (!this.state.isLogged) return null; // Não renderiza se o usuário não está logado
+    if (!this.state.usuario_logado) return null; // Não renderiza nada se o usuário não estiver logado
 
+    // Definindo os filmes, agora com o caminho relativo para imagens locais
     const movies = [
-      { id: '1', title: 'Inception' },
-      { id: '2', title: 'The Matrix' },
-      { id: '3', title: 'Interstellar' },
+      { id: '1', title: 'Suicidas', image: require('./img/SUICIDAS.jpg') }, // Imagem local
+      { id: '2', title: 'Diário de um banana' ,image: require('./img/diario_banana.jpg') }, // Imagem local
+      { id: '3', title: 'Pequeno Príncipe' ,image: require('./img/pequeno_principe.jfif') },
+      { id: '4', title: 'A Culpa é das Estrelas' ,image: require('./img/culpa_estrela.jfif') },
+      { id: '5', title: 'A Cabana' ,image: require('./img/cabana.jpg') }, 
     ];
 
+    // Ordenando os filmes pelo número de comentários (ranking)
+    const sortedMovies = [...movies].sort((a, b) => {
+      const commentsA = this.state.comments[a.id] || [];
+      const commentsB = this.state.comments[b.id] || [];
+      return commentsB.length - commentsA.length; // Maior número de comentários primeiro
+    });
+
     return (
-      <View>
+      <ScrollView style={{ flex: 1 }}>
+        {/* Exibindo os filmes com seus comentários */}
         <FlatList
-          data={movies}
+          data={sortedMovies}
           keyExtractor={(item) => item.id}
           renderItem={({ item }) => (
-            <View>
-              <Text>{item.title}</Text>
+            <View style={{ marginBottom: 20, alignItems: 'center' }}>
+              <Text style={{ fontSize: 18, fontWeight: 'bold' }}>{item.title}</Text>
+              {/* Exibindo imagem do filme */}
+              <Image source={item.image} style={{ width: 200, height: 300, marginBottom: 10 }} />
+              {/* Campo de entrada de comentário */}
               <TextInput
                 placeholder="Escreva um comentário"
                 value={this.state.comment}
                 onChangeText={(text) => this.setState({ comment: text })}
+                style={{
+                  height: 40,
+                  borderColor: 'gray',
+                  borderWidth: 1,
+                  marginBottom: 10,
+                  paddingLeft: 10,
+                  width: 250,
+                }}
               />
+              {/* Botão para adicionar comentário */}
               <Button title="Comentar" onPress={() => this.addComment(item.id)} />
-              <Text>Comentários:</Text>
+              {/* Exibe o número de comentários */}
+              <Text>Comentarios: {this.state.comments[item.id]?.length || 0}</Text>
+              {/* Exibe os comentários do filme */}
               {(this.state.comments[item.id] || []).map((c, index) => (
-                <Text key={index}>{c}</Text>
+                <Text key={index} style={{ marginTop: 5 }}>{c}</Text>
               ))}
             </View>
           )}
+          contentContainerStyle={{ paddingBottom: 20 }} // Adicionando padding extra no final para melhorar a rolagem
         />
-      </View>
+      </ScrollView>
     );
   }
 }
@@ -192,10 +231,10 @@ class App extends React.Component {
             }}
           />
           <Tab.Screen
-            name="Filmes"
-            component={Filmes}
+            name="Livros"
+            component={Livros}
             options={{
-              tabBarIcon: ({ color, size }) => (<MaterialCommunityIcons name="movie" color={color} size={size} />)
+              tabBarIcon: ({ color, size }) => (<MaterialCommunityIcons name="book" color={color} size={size} />)
             }}
           />
         </Tab.Navigator>
